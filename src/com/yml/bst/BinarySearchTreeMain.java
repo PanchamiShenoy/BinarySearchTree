56287184e617c1fbe8b2dcf5fc2133922b8f4d4d
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-15 13:05:16 +0530

    binary search tree is created

---
+++ src/com/yml/bst/BinarySearchTreeMain.java
@@ -0,0 +1,14 @@
+package com.yml.bst;
+
+public class BinarySearchTreeMain {
+
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.printNodes();
+
+	}
+
+}
